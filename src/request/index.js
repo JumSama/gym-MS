8f83acf5f4fe8cf6d52f3d80297fede0eaa1c776
JumSama <1274001055@qq.com>
2022-03-14 23:07:23 +0800

    feat:二次封装axios

---
+++ src/request/index.js
@@ -0,0 +1,27 @@
+import JumRequest from './request'
+
+// 请求拦截器
+const requestInterceptors = {
+  success(value) {
+    return value
+  },
+  error(err) {
+    console.log(err)
+  }
+}
+
+// 响应拦截器
+const responseInterceptors = {
+  success(value) {
+    return value
+  },
+  error(err) {
+    console.log(err)
+  }
+}
+
+export default new JumRequest({
+  baseURL: 'http://127.0.0.1:8800',
+  requestInterceptors,
+  responseInterceptors
+})
